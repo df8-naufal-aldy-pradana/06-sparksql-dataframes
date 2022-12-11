9bf1d3917e9d83d4634b5733db0b7756e33b3c26
blitzkz23 <blitzaldykazu@gmail.com>
2022-12-11 01:30:54 +0000

    feat: some minor changes

--- airflow-spark/bin/download_data.sh
+++ airflow-spark/bin/download_data.sh
@@ -1,4 +1,4 @@
-URL="https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2022-02.parquet"
+URL="https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2021-02.parquet"
 LOCAL_PATH="./spark/resources/data/"
 wget ${URL} -P ${LOCAL_PATH} 
 
